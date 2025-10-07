379ad5dbaff47ce48404979ef51ed584aca21816
liulei <x157123123@gmail.com>
2025-10-08 01:02:41 +0800

    代码提交

--- src/app/layouts/full/sidebar/sidebar.component.ts
+++ src/app/layouts/full/sidebar/sidebar.component.ts
@@ -51,14 +51,10 @@ const menuItems = [
   {state: false, key: 'index1',path: 'user', name: 'User', type: 'link', icon: 'av_timer', items: []},
   {state: false, key: 'index3',path: 'flex', name: 'flex', type: 'link', icon: 'av_timer', items: []},
   {
-    state: false, key: 'index22',path: 'index22', name: 'Indexssss', type: 'link', icon: 'home', items: [
-      {
-        state: false, key: 'index23',path: 'index23', name: 'index', type: 'link', icon: 'home', items: [
-          {state: false, key: 'index24',path: 'index', name: '数据库配置', type: 'link', icon: 'av_timer'},
-          {state: false, key: 'deploy',path: 'deploy', name: '同步配置', type: 'link', icon: 'av_timer'},
-          {state: false, key: 'login',path: 'login', name: 'login', type: 'link', icon: ''},
-        ]
-      }
+    state: false, key: 'index23',path: 'index23', name: 'index', type: 'link', icon: 'home', items: [
+      {state: false, key: 'index24',path: 'index', name: '数据库配置', type: 'link', icon: 'av_timer'},
+      {state: false, key: 'deploy',path: 'deploy', name: '同步配置', type: 'link', icon: 'av_timer'},
+      {state: false, key: 'login',path: 'login', name: 'login', type: 'link', icon: ''},
     ]
   },
   {state: false, key: 'help',path: 'help', name: '帮助', type: 'link', icon: 'av_timer', items: []},
